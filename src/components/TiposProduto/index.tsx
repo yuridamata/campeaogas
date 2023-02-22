a5563e3373a2282d522fdb2ddd4085930b2c8698
Yuri da Mata <yuridamata@gmail.com>
2023-02-22 08:14:24 -0300

    Novas seções

--- src/components/TiposProduto/index.tsx
+++ src/components/TiposProduto/index.tsx
@@ -1,9 +1,24 @@
+import Image from "next/image";
 import styles from "./tiposProdutos.module.scss";
 
 export default function TiposProdutos(){
-  return <section>
-    <div className={styles.tiposProdutos}>
-    TiposProdutos
+  return <section className={styles.tiposProdutos}>
+    <div className={styles.texto}>  
+      <h1>
+        Feito como melhor material
+      </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum molestie ultrices imperdiet. Duis dolor Leo, sollicitudin non nulla congue, auctor euismod orci. Cras ornare diam vel leo hendrerit sollicitudin. Nam nec sollicitudin quam. Donec ac purus et arcu pharetra elementum. Aliquam tempus eleifend ligula id finibus.
+      </p>
+      <button>
+        Saiba Mais
+      </button>
+    </div>
+    <div className={styles.tipos}>
+      <Image src="/tiposBotijao/p13.png" alt="Imagem do botijão p13" width="233" height="370"/>
+      <Image src="/tiposBotijao/p20.png" alt="Imagem do botijão p13" width="233" height="370"/>
+      <Image src="/tiposBotijao/p45.png" alt="Imagem do botijão p13" width="233" height="370"/>
+      
     </div>
   </section>
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,8 +26,8 @@ export default function Home() {
         <Header />
         <Hero />
         <QuemSomos />
-        {/* <TiposProdutos />
-        <Caracteristicas />
+         <TiposProdutos />
+        {/*<Caracteristicas />
         <Promocao />
         <MaisProdutos />
         <Lojas />
